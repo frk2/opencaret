1c0584685f730ed633f9fd6b9b67fdf849683257
Faraz Khan <farazrkhan@gmail.com>
2018-06-21 13:35:20 -0700

    - Use latest launch system for ROS2

--- src/launch/all_launch.py
+++ src/launch/all_launch.py
@@ -1,19 +1,12 @@
-from launch.legacy.exit_handler import primary_exit_handler
-from ros2run.api import get_executable_path
+from launch import LaunchDescription
+import launch_ros.actions
 
-def launch(launch_descriptor, argv):
-    ld = launch_descriptor
 
-    ld.add_process(
-        cmd=[get_executable_path(package_name='canoc', executable_name='transceiver')]
-    )
-
-    ld.add_process(
-        cmd=[get_executable_path(package_name='radar', executable_name='radar_controller')]
-    )
-
-    # ld.add_process(
-    #     cmd=[get_executable_path(
-    #         package_name=package, executable_name='lifecycle_service_client')],
-    #     exit_handler=primary_exit_handler,
-    #)
+def generate_launch_description():
+    """Launch a talker and a listener."""
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='canoc', node_executable='transceiver', output='screen'),
+        launch_ros.actions.Node(
+            package='radar', node_executable='radar_controller', output='screen'),
+])
