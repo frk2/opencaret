6949e2bedefbfee4e4043946985f5776a5f1e367
jrojas <jrojas@redlinesolutions.co>
2018-08-15 11:14:09 -0700

    Fixes for ros1 bridge compilation wrt zed_wrapper

--- ros1_bridge/build.sh
+++ ros1_bridge/build.sh
@@ -1,4 +1,17 @@
+function finish {
+
+  popd
+  #re-enable zed 
+  mv ../ros1_ws/src/.zed-ros-wrapper ../ros1_ws/src/zed-ros-wrapper
+}
+
+trap finish EXIT
+
 source ./source.sh
-cd $ROS2_ROOT
+
+# hide zed_wrapper (it is not yet compatible with melodic since it uses a missing opencv3 package)
+mv ../ros1_ws/src/zed-ros-wrapper ../ros1_ws/src/.zed-ros-wrapper
+
+pushd $ROS2_ROOT
 rm -rf build/ros1_bridge
 colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
--- ros1_bridge/source.sh
+++ ros1_bridge/source.sh
@@ -1,4 +1,4 @@
 source $ROS1_ROOT/setup.bash
-. $ROS2_ROOT/install/local_setup.bash
+source $ROS2_ROOT/install/setup.bash
 source ../ros1_ws/devel/setup.bash
-. ../ros2_ws/install/local_setup.bash
+source ../ros2_ws/install/local_setup.bash
