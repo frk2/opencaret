b08aa9296bdb469c39c2555f33cd2965a17df11b
jrojas <jrojas@redlinesolutions.co>
2018-08-16 08:47:47 -0700

    Remove record_bag_libav.sh (this has been superceded by record_bag.sh and launch_ros1.sh) Add helpful messages in ros1_bridge/source.sh

--- ros1_bridge/source.sh
+++ ros1_bridge/source.sh
@@ -1,3 +1,13 @@
+if [ "$ROS1_ROOT" == "" ]; then
+  echo "ROS1_ROOT not set. Please set this environment variable to your system's ROS1 path."
+  exit 1
+fi
+
+if [ "$ROS2_ROOT" == "" ]; then
+  echo "ROS2_ROOT not set. Please set this environment variable to your system's ROS2 workspace."
+  exit 1
+fi
+
 source $ROS1_ROOT/setup.bash
 source $ROS2_ROOT/install/setup.bash
 source ../ros1_ws/devel/setup.bash
