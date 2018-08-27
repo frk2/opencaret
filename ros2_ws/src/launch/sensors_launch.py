78a696aa23800eb8157d878883968bb3adc289c6
jrojas <jrojas@redlinesolutions.co>
2018-08-26 20:58:23 -0700

    Add sensors only launch

---
+++ ros2_ws/src/launch/sensors_launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+import launch_ros.actions
+import os
+
+def generate_launch_description():
+
+    current_path = os.path.dirname(os.path.realpath(__file__))
+
+    """Launch a talker and a listener."""
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='vehicle', node_executable='kia_soul_driver', output='screen'),
+        launch_ros.actions.Node(
+            package='radar', node_executable='radar_controller', output='screen'),
+        launch_ros.actions.Node(
+            package='vehicle', node_executable='kia_soul_driver', output='screen'),
+        launch_ros.actions.Node(
+            package='canoc', node_executable='transceiver', arguments=['can0'], output='screen'),
+        launch_ros.actions.Node(
+            package='canoc', node_executable='transceiver', arguments=['can1'], output='screen')
+    ])
