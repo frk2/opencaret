76b40415ab440661131eec5ee0ff6cf03928333e
Faraz Khan <farazrkhan@gmail.com>
2018-08-29 19:00:12 -0700

    - Remove TF publishers from ros2 launch

--- ros2_ws/src/launch/all_launch.py
+++ ros2_ws/src/launch/all_launch.py
@@ -19,21 +19,5 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='canoc', node_executable='transceiver', arguments=['can0'], output='screen'),
         launch_ros.actions.Node(
-            package='canoc', node_executable='transceiver', arguments=['can1'], output='screen'),
-        launch_ros.actions.Node(
-            package='robot_state_publisher', node_executable='robot_state_publisher',
-            arguments=[os.path.join(current_path,'../../../data/kia_soul/robot_description.urdf')]
-        ),
-        launch_ros.actions.Node(
-            package='tf2_ros', node_executable='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link']
-        ),
-        launch_ros.actions.Node(
-            package='tf2_ros', node_executable='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', 'front_camera_link', 'zed_left_camera']
-        ),
-        launch_ros.actions.Node(
-            package='tf2_ros', node_executable='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', 'front_camera_link', 'zed_depth_camera']
-        )
+            package='canoc', node_executable='transceiver', arguments=['can1'], output='screen')
     ])
