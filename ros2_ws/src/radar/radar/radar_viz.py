37f3b42b0f43e52dd95ed9584736f10ca38a6980
Faraz Khan <farazrkhan@gmail.com>
2018-09-25 20:08:09 -0700

    - Mark lead obstacle

--- ros2_ws/src/radar/radar/radar_viz.py
+++ ros2_ws/src/radar/radar/radar_viz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from util import rospy_compat
-from opencaret_msgs.msg import RadarTracks
+from opencaret_msgs.msg import RadarTracks, Obstacle
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
@@ -13,6 +13,26 @@ class RadarViz(rospy_compat.Node):
         rospy_compat.init_node(self, 'radar_viz')
         self.radar_sub = rospy_compat.Subscriber('/radar_tracks', RadarTracks, self.on_radar_tracks)
         self.radar_rviz_pub = rospy_compat.Publisher('/radar_viz', Marker, queue_size=1)
+        self.lo_pub = rospy_compat.Subscriber('/lead_obstacle', Obstacle, self.on_lead_obstacle)
+
+    def on_lead_obstacle(self, msg):
+        marker = Marker()
+        marker.header.frame_id = "middle_radar_link"
+        if rospy_compat.use_ros_1:
+            marker.header.stamp = rospy_compat.rospy.Time.now()
+        marker.ns = "lead_obstacle"
+        marker.id = 1
+        marker.type = Marker.POINTS
+        marker.action = Marker.MODIFY
+        marker.scale.x = 0.5
+        marker.scale.y = 0.5
+        marker.scale.z = 0.1
+        marker.color.r = 1.0
+        marker.color.a = 1.0
+        msg.point.y *= -1
+        marker.points = [msg.point]
+        marker.colors = [ColorRGBA(0.,0.,1.0,1.0)]
+        self.radar_rviz_pub.publish(marker)
 
     def on_radar_tracks(self, msg):
         marker = Marker()
