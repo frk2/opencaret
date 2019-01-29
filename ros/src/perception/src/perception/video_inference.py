ac3f8c12f0fd7eb605e65df54e763d3413dbeb51
Faraz Khan <farazrkhan@gmail.com>
2019-01-29 15:17:13 -0800

    - Perception stack using ESPNet, with sample data and video_inference

---
+++ ros/src/perception/src/perception/video_inference.py
@@ -0,0 +1,30 @@
+import sys
+from moviepy.editor import *
+import perception.predict as predict
+import numpy as np
+import cv2
+
+def main():
+    if len(sys.argv) != 2:
+        print("Please specify path to movie file.")
+        exit()
+
+    filepath = sys.argv[1]
+    print("Reading movie file: {0}".format(filepath))
+
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+    # out = cv2.VideoWriter('output.mp4', fourcc, 10.0, (1920, 1080))
+    predictor = predict.Prediction()
+
+    def process_image(img):
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        prediction = predictor.infer(img.astype(np.float32), overlay=True)
+        return np.array(prediction)
+
+    video = VideoFileClip(filepath).subclip(50,100).fl_image(process_image)
+    video.write_videofile('output.mp4')
+    print("\nFINISH")
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros/src/perception/src/perception/zed_svo_inference.py
@@ -0,0 +1,59 @@
+import sys
+import pyzed.camera as zcam
+import pyzed.types as tp
+import pyzed.core as core
+import perception.predict as predict
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from cuda_context import PyCudaContext
+# obtain cuda-fun and install from: https://github.com/frk2/cuda-fun
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Please specify path to .svo file.")
+        exit()
+
+    filepath = sys.argv[1]
+    print("Reading SVO file: {0}".format(filepath))
+
+    init = zcam.PyInitParameters(svo_input_filename=filepath, camera_image_flip=False, svo_real_time_mode=False)
+
+    cam = zcam.PyZEDCamera()
+    status = cam.open(init)
+    zed_cuda_ctx=PyCudaContext()
+
+    zed_cuda_ctx.pop_ctx()
+    predictor = predict.Prediction()
+    zed_cuda_ctx.push_ctx()
+
+    if status != tp.PyERROR_CODE.PySUCCESS:
+        print(repr(status))
+        exit()
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+    out = cv2.VideoWriter('output.mp4', fourcc, 10.0, (1920, 1080))
+
+    runtime = zcam.PyRuntimeParameters()
+    mat = core.PyMat()
+    while True:  # for 'q' key
+        err = cam.grab(runtime)
+        if err == tp.PyERROR_CODE.PySUCCESS:
+            cam.retrieve_image(mat)
+            npimg = mat.get_data().astype(np.float32)[:,:,:3]
+            zed_cuda_ctx.pop_ctx()
+            prediction = predictor.infer(npimg, overlay=True)
+            zed_cuda_ctx.push_ctx()
+            out_image = cv2.cvtColor(np.array(prediction), cv2.COLOR_RGB2BGR)
+            cv2.imshow('Perception', out_image)
+            out.write(out_image)
+            key = cv2.waitKey(1)
+    cv2.destroyAllWindows()
+    out.release()
+    cam.close()
+    print("\nFINISH")
+
+
+if __name__ == '__main__':
+    main()
